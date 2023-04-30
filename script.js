e2390122702f3fc8e60ec1e5c25352ce219b873b
kmamounis <132182042+kmamounis@users.noreply.github.com>
2023-04-29 23:44:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 'use strict';
 
-var catImage = document.getElementById('cat-image');
-var newCatButton = document.getElementById('new-cat-button');
+const catImage = document.getElementById('cat-image');
+const newCatButton = document.getElementById('new-cat-button');
 
 function fetchCatImage() {
   fetch('https://api.thecatapi.com/v1/images/search')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      var imageUrl = data[0].url;
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data[0].url;
       catImage.src = imageUrl;
     })
-    .catch(function(error) {
-      console.error(error);
-    });
+    .catch(error => console.error(error));
 }
 
 newCatButton.addEventListener('click', function() {
